a54ba1ba78c2b73cbf281ba7babaa2852dd9eb02
Andrey Stolbovsky <andrey.stolbovsky@gmail.com>
2016-09-08 00:01:41 +0300

    Refactor structure, add config

--- receiver/receiver.go
+++ receiver/receiver.go
@@ -5,8 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"reflect"
 	"strings"
 	"sync"
+	"time"
+
+	"menteslibres.net/gosexy/yaml"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -17,27 +21,71 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
 
-	svc := sqs.New(session.New(), &aws.Config{Region: aws.String("eu-west-1")})
-	go receiveMessageLoop(svc, "TestQueue")
-	go receiveMessageLoop(svc, "TestQueue2")
+	settings, err := yaml.Open("config.yml")
+	if err != nil {
+		log.Fatalf("Cannot read config: %v\n", err.Error())
+		return
+	}
+
+	svc := createSvc()
+
+	err2 := launchLoops(settings, svc)
+	if err2 != nil {
+		log.Fatalf(err2.Error())
+		return
+	}
+
 	wg.Wait()
 }
 
-func receiveMessageLoop(svc *sqs.SQS, queueName string) {
+func createSvc() (service *sqs.SQS) {
+	svc := sqs.New(session.New(), &aws.Config{Region: aws.String("eu-west-1")})
 
+	return svc
+}
+
+func launchLoops(settings *yaml.Yaml, svc *sqs.SQS) error {
+	queues := settings.Get("queues")
+
+	queuesType := reflect.TypeOf(queues).Kind()
+	if queuesType != reflect.Slice {
+		return fmt.Errorf("Queues config entry should be an array of strings, it is %v", queuesType)
+	}
+
+	for _, queue := range queues.([]interface{}) {
+		queueType := reflect.TypeOf(queue).Kind()
+		if queueType != reflect.String {
+			return fmt.Errorf("Queue entry should be a string, it is %v", queueType)
+		}
+		go receiveMessageLoop(svc, queue.(string))
+	}
+
+	return nil
+}
+
+func receiveMessageLoop(svc *sqs.SQS, queueName string) {
 	for {
-		receiveMessage(svc, queueName)
+		processMessage(svc, queueName)
 	}
 }
 
-func receiveMessage(svc *sqs.SQS, queueName string) {
+func processMessage(svc *sqs.SQS, queueName string) {
+	message := receiveMessage(svc, queueName)
+	if message == nil {
+		return
+	}
 
-	log.Println("Receiving new messages... " + queueName)
+	runWorker(*message.Body, queueName)
+	deleteMessage(svc, message, queueName)
+}
+
+func receiveMessage(svc *sqs.SQS, queueName string) (message *sqs.Message) {
+	log.Println("[" + queueName + "] Waiting for new messages... ")
 
 	receiveMessageParams := &sqs.ReceiveMessageInput{
 		QueueUrl:          aws.String("https://sqs.eu-west-1.amazonaws.com/245915766340/" + queueName), // Required
 		VisibilityTimeout: aws.Int64(100),
-		WaitTimeSeconds:   aws.Int64(10),
+		WaitTimeSeconds:   aws.Int64(20),
 	}
 
 	resp, err := svc.ReceiveMessage(receiveMessageParams)
@@ -48,27 +96,38 @@ func receiveMessage(svc *sqs.SQS, queueName string) {
 	}
 
 	if len(resp.Messages) < 1 {
-		log.Println("No new messages in " + queueName)
+		log.Println("[" + queueName + "] No new messages")
 		return
 	}
 
-	log.Println("Got new message from " + queueName)
+	log.Println("[" + queueName + "] Got new message")
 
-	message := resp.Messages[0]
+	return resp.Messages[0]
+}
 
-	cmd := exec.Command("php", "worker.php")
-	var out bytes.Buffer
-	cmd.Stdin = strings.NewReader(*message.Body)
-	cmd.Stdout = &out
-	err2 := cmd.Run()
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-	fmt.Printf("%v", out.String())
-
+func deleteMessage(svc *sqs.SQS, message *sqs.Message, queueName string) {
 	deleteMessageParams := &sqs.DeleteMessageInput{
 		QueueUrl:      aws.String("https://sqs.eu-west-1.amazonaws.com/245915766340/" + queueName), // Required
 		ReceiptHandle: message.ReceiptHandle,
 	}
 	svc.DeleteMessage(deleteMessageParams)
+}
+
+func runWorker(payload string, queueName string) {
+	start := time.Now()
+
+	log.Printf("[" + queueName + "] Running worker\n")
+
+	cmd := exec.Command("php", "worker.php")
+	var out bytes.Buffer
+	cmd.Stdin = strings.NewReader(payload)
+	cmd.Stdout = &out
+	err2 := cmd.Run()
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+	log.Printf("["+queueName+"] Worker output: %v", out.String())
+
+	timeSpent := time.Since(start)
+	log.Printf("["+queueName+"] Ran worker in %.3f\n", timeSpent.Seconds())
 }
